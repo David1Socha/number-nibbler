f24582cb0124ca06b227b4ffa4492788f9dac94f
David Socha <David1Socha@gmail.com>
2015-03-09 17:45:16 -0500

    Add delayed movement

--- main.lua
+++ main.lua
@@ -2,11 +2,16 @@ function love.load()
   scale = 64
   grid_size = 4
   player = build_player()
-  select = love.audio.newSource("select.ogg", "static")
+  select = love.audio.newSource("assets/select.ogg", "static")
 end
 
 function love.update(dt)
-
+  player.time_moving = player.time_moving + dt
+  if player.time_moving > player.movetime then
+    player.time_moving = player.time_moving - player.movetime
+    player.x = move_closer(player.x, player.dx, 1)
+    player.y = move_closer(player.y, player.dy, 1)
+  end
 end
 
 function love.mousepressed(x, y, button)
@@ -21,7 +26,18 @@ function love.mousepressed(x, y, button)
 end
 
 function love.draw()
+  --love.graphics.printf(msg, 100, 100, 100)
   love.graphics.rectangle("fill", (player.x * scale) + player.size / 2, (player.y * scale) + player.size / 2, player.size, player.size)
+end
+
+function move_closer(current, dest, step)
+  local new = nil
+  if current > dest then
+    new = math.max(current - step, dest)
+  else
+    new = math.min(current + step, dest)
+  end
+  return new
 end
 
 function choose_square()
@@ -32,7 +48,10 @@ function build_player()
   local _player = {
     x = 0,
     y = 0,
-    speed = 10,
+    dx = 0,
+    dy = 0,
+    time_moving = 0,
+    movetime = .2,
     size = 32
   }
 
