92ca7e5a6826451303b9d05c92e8bddf193a64e2
David Socha <David1Socha@gmail.com>
2015-05-05 19:09:16 -0500

    Remove debug library

--- main.lua
+++ main.lua
@@ -3,35 +3,24 @@ menu = require("menu")
 vector = require("hump.vector")
 game = require("game")
 Timer = require "hump.timer"
-require "monocle.monocle"
 require "equality"
-Monocle.new({})
 
 function love.load()
   Gamestate.registerEvents()
   Gamestate.switch(menu)
   d = 0
-  Monocle.watch("dt", function() return d end)
-  Monocle.watch("player pos", function() if game.player then return game.player.pos end end)
-  Monocle.watch("player dest", function() if game.player then return game.player.dest end end)
-  Monocle.watch("player act", function() if game.player then return game.player.act end end)
-  Monocle.watch("yes flies", function() return game.yes_flies end)
 end
 
 function love.update(dt)
   d=dt
-  Monocle.update()
   Timer.update(dt)
 end
 
 function love.draw()
-  Monocle.draw()
 end
 
 function love.textinput(t)
-  Monocle.textinput(t)
 end
 
 function love.keypressed(text)
-  Monocle.keypressed(text)
 end
