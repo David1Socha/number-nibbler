2445c56aed122d07a80b827ec6af38c80c5d4d3f
David Socha <david1socha@gmail.com>
2015-06-07 13:17:07 -0500

    Create title text

--- menu.lua
+++ menu.lua
@@ -2,7 +2,8 @@ menu = {}
 
 function menu:enter()
   menu.font = love.graphics.newFont("assets/font/kenvector_future_thin.ttf",54)
-  menu.bgcolor = {20, 60, 20}
+  menu.bgcolor = {255,255,153}
+  menu.title = love.graphics.newImage("assets/image/title.png")
 end
 
 function menu:draw() 
@@ -10,6 +11,7 @@ function menu:draw()
   love.graphics.setFont(menu.font)
   love.graphics.setColor({0xff, 0xff, 0xff})
   love.graphics.printf("Press to start", 0, love.graphics.getHeight() / 2 - 100, love.graphics.getWidth(), "center")
+  love.graphics.draw(self.title,150,10,0,1,1)
 end
 
 function menu:mousepressed()
