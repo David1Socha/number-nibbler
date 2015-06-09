6e1d3cb7b60054318950dae010d7212bced50af9
David Socha <david1socha@gmail.com>
2015-06-08 20:37:13 -0500

    Add loveframes library

--- main.lua
+++ main.lua
@@ -1,14 +1,12 @@
-Gamestate = require "hump.gamestate"
-menu = require "menu"
-vector = require "hump.vector"
-game = require "game"
-Timer = require "hump.timer"
-require "equality"
-
-math.randomseed(os.time())
-
-
 function love.load()
+  Gamestate = require "hump.gamestate"
+  menu = require "menu"
+  vector = require "hump.vector"
+  game = require "game"
+  Timer = require "hump.timer"
+  loveframes = require("loveframes")
+  require "equality"
+  math.randomseed(os.time())
   line_width = 0.0078125 * love.window.getWidth()
   Gamestate.registerEvents()
   love.graphics.setLineWidth(line_width)
@@ -20,7 +18,17 @@ end
 function love.update(dt)
   d=dt
   Timer.update(dt)
+  loveframes.update(dt)
 end
 
 function love.draw()
+  loveframes.draw()
+end
+
+function love.mousepressed(x,y,button)
+  loveframes.mousepressed(x,y,button)
+end
+
+function love.mousereleased(x, y, button)
+  loveframes.mousereleased(x, y, button)
 end
