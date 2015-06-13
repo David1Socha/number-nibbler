c5c1691af130a202d6254637a372109a73cf60fd
David Socha <david1socha@gmail.com>
2015-06-13 10:42:11 -0500

    Add function to generate subtraction traps

--- answer.lua
+++ answer.lua
@@ -37,3 +37,20 @@ function gen_subtraction_answers(x)
   end
   return as
 end
+
+function gen_subtraction_traps(x)
+  local ts = {}
+  local as = gen_subtraction_answers(x)
+  table.foreach(as, function(i)
+    a = as[i]
+    for i=a[1]-MIN_DELTA,a[1]+MAX_DELTA do --range of near values to try in order to trick player
+      for j=a[2]-MIN_DELTA,a[2]+MAX_DELTA do
+        if (i>0 and j>0 and i-j~=x) then --don't want to give away the falseness by going out of original range
+          local t = {i,j}
+          print(t[1],t[2])
+          table.insert(ts, t)
+        end
+      end
+    end
+  end)
+end
