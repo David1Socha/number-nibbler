40b5219ee91b241fb676ead1e245fa43cc09d26a
David Socha <david1socha@gmail.com>
2015-06-13 15:03:41 -0500

    Add category select buttons to menu

--- menu.lua
+++ menu.lua
@@ -2,7 +2,7 @@ menu = {}
 menu.width = love.graphics.getWidth()
 menu.height = love.graphics.getHeight()
 menu.start_button_x = 0.25 * menu.width
-menu.start_button_y = .18 * menu.width
+menu.start_button_y = .14 * menu.width
 menu.start_button_w = 0.5 * menu.width
 menu.start_button_h = .075 * menu.width
 menu.start_button_fontsize = .04 * menu.width
@@ -21,15 +21,28 @@ function menu:enter()
     font=menu.start_button_font,
     text="Play Game",
     onclick=menu.start_game,
+    outline_width=.00391*menu.width,
   }
 
   menu.category_left_button = menu.mgr:new_button {
-    x=600,
-    y=400,
+    x=0.0078 * menu.width,
+    y=0.453 * menu.width,
     text="",
     image=love.graphics.newImage("assets/image/button_left.png"),
     outline_width=0,
   }
+
+  menu.category_left_button = menu.mgr:new_button {
+    x=.461*menu.width,
+    y=0.453 * menu.width,
+    text="",
+    image=love.graphics.newImage("assets/image/button_right.png"),
+    outline_width=0,
+  }
+
+  menu.name_font = love.graphics.newFont("assets/font/kenvector_future_thin.ttf",.0469*menu.width)
+  menu.category = Categories.ADDITION
+  menu.category_name = "Multiplication"
 end
 
 function menu.start_game()
@@ -44,6 +57,11 @@ function menu:draw()
   love.graphics.setBackgroundColor(menu.bgcolor)
   love.graphics.setColor({0xff, 0xff, 0xff})
   love.graphics.draw(self.title,.5*menu.width,0.01*menu.height,0,menu.title_scale,menu.title_scale,menu.title:getWidth()*.5,0)
+
+  love.graphics.setColor {0,0,0}
+  love.graphics.setFont(self.name_font)
+  love.graphics.printf(self.category_name,.063*menu.width,.469*menu.width,.39*menu.width,"center")
+
   menu.mgr:draw()
 end
 
