cbd0a0e75b83033173c2c5027e47f3ab8027a722
David Socha <david1socha@gmail.com>
2015-06-14 11:26:43 -0500

    Add logic to generate multiplication answers/traps

--- answer.lua
+++ answer.lua
@@ -3,8 +3,8 @@ local MAX_DELTA = 1
 
 function gen_addition_answers(x)
   local as = {}
-  for i=1,x/2 do
-    local a = {i, x-i}
+  for addend=1,x/2 do
+    local a = {addend, x-addend}
     table.insert(as, a)
   end
   return as
@@ -54,3 +54,39 @@ function gen_subtraction_traps(x)
   end)
   return ts
 end
+
+function gen_multiplication_answers(x)
+  local as = {}
+  local prev_large_factor = x
+  for small_factor=1,math.sqrt(x) do
+    for large_factor=prev_large_factor,small_factor,-1 do
+      if small_factor * large_factor == x then
+        local a = {small_factor, large_factor}
+        table.insert(as, a)
+        break
+      end
+    end
+  end
+  table.foreach(as, function(k,v) print(v[1],v[2]) end)
+  print("  ")
+  return as
+end
+
+function gen_multiplication_traps(x)
+  local ts = {}
+  local as = gen_multiplication_answers(x)
+  for k,a in ipairs(as) do
+    for i=a[1]-MIN_DELTA,a[1]+MAX_DELTA do
+      for j=a[2]-MIN_DELTA,a[2]+MAX_DELTA do
+        if (i>0 and j>0 and i*j~=x) then
+          local t = {i,j}
+          table.insert(ts,t)
+        end
+      end
+    end
+  end
+  table.foreach(ts, function(k,v) print(v[1],v[2]) end)
+  return ts
+end
+
+gen_multiplication_traps(20)
