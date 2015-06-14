e3e4ab44e4a85d2865506bcb26a970cb1b692994
David Socha <david1socha@gmail.com>
2015-06-14 11:38:48 -0500

    Add categories file

---
+++ categories.lua
@@ -0,0 +1,14 @@
+local Categories = {}
+
+Categories.NUM_CATEGORIES = 3
+Categories.ADDITION = 1
+Categories.SUBTRACTION = 2
+Categories.MULTIPLICATION = 3
+
+Categories.Names = {}
+
+Categories.Names[Categories.ADDITION] = "Addition"
+Categories.Names[Categories.SUBTRACTION] = "Subtraction"
+Categories.Names[Categories.MULTIPLICATION] = "Multiplication"
+
+return Categories
