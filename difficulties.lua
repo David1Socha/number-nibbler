2422ad9b3d84b9aa326058662d5741b7b09e91cf
David Socha <david1socha@gmail.com>
2015-08-02 12:26:53 -0500

    Add difficulty values

---
+++ difficulties.lua
@@ -0,0 +1,12 @@
+local Difficulties = {}
+
+Difficulties.NUM_DIFFICULTIES = 2
+Difficulties.EASY = 1
+Difficulties.HARD = 2
+
+Difficulties.Names = {}
+
+Difficulties.Names[Difficulties.EASY] = "Easy"
+Difficulties.Names[Difficulties.HARD] = "Hard"
+
+return Difficulties
