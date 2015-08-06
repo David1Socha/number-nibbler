487bc49a029f2cbbf97f7f7dba714497d3e1cf0e
David Socha <david1socha@gmail.com>
2015-08-06 16:18:35 -0500

    Add multiples to categories

--- categories.lua
+++ categories.lua
@@ -4,11 +4,13 @@ Categories.NUM_CATEGORIES = 3
 Categories.ADDITION = 1
 Categories.SUBTRACTION = 2
 Categories.MULTIPLICATION = 3
+Categories.MULTIPLES = 4
 
 Categories.Names = {}
 
 Categories.Names[Categories.ADDITION] = "Addition"
 Categories.Names[Categories.SUBTRACTION] = "Subtraction"
 Categories.Names[Categories.MULTIPLICATION] = "Multiplication"
+Categories.Names[Categories.MULTIPLES] = "Multiples"
 
 return Categories
